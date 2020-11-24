5189f8aa9bf4d3e9ad5103482330b5a41e19a627
Paulofalcao2002 <paulohenriquemoreirafalcao@gmail.com>
2020-11-24 20:32:17 -0300

    completa load_assets()

--- codigoinicial.py
+++ codigoinicial.py
@@ -27,3 +27,20 @@ def load_assets():
     assets['carro2_mao'] = pygame.transform.scale(carro2_mao, (80, 40))
     assets['carro3_mao'] = pygame.transform.scale(carro3_mao, (80, 40))
     assets['carro4_mao'] = pygame.transform.scale(carro4_mao, (80, 40))
+
+    carro1_contramao = pygame.image.load('assets/carros/carro1_contramao.png').convert_alpha()
+    carro2_contramao = pygame.image.load('assets/carros/carro2_contramao.png').convert_alpha()
+    carro3_contramao = pygame.image.load('assets/carros/carro3_contramao.png').convert_alpha()
+    carro4_contramao = pygame.image.load('assets/carros/carro4_contramao.png').convert_alpha()
+    assets['carro1_contramao'] = pygame.transform.scale(carro1_contramao, (80, 40))
+    assets['carro2_contramao'] = pygame.transform.scale(carro2_contramao, (80, 40))
+    assets['carro3_contramao'] = pygame.transform.scale(carro3_contramao, (80, 40))
+    assets['carro4_contramao'] = pygame.transform.scale(carro4_contramao, (80, 40))
+
+    pygame.mixer.music.load('assets/sons/background.ogg')
+    pygame.mixer.music.set_volume(0.4)
+    assets['som_ponto'] = pygame.mixer.Sound('assets/sons/ponto.mpeg')
+    assets['som_batida'] = pygame.mixer.Sound('assets/sons/batida.mpeg')
+    assets['som_fim'] = pygame.mixer.Sound('assets/sons/fatality.mp3')
+
+    return assets
