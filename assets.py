6c446b1be037bef721758d805dcf1abe31e90a0f
Paulofalcao2002 <paulohenriquemoreirafalcao@gmail.com>
2020-11-25 20:17:48 -0300

    adicionando comentários

--- assets.py
+++ assets.py
@@ -1,12 +1,19 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
 import pygame
 from constantes import WIDTH_CARRINHO, HEIGHT_CARRINHO, WIDTH_GALINHA, HEIGHT_GALINHA
+
+#função que carrega os assets
 def load_assets():
+    #Criando um dicionário que armazanerá os assets
     assets = {}
+    #Carregando as fontes
     assets['font_pontos'] = pygame.font.Font('assets/PressStart2P.ttf', 15)
     assets['font_gameover'] = pygame.font.Font('assets/PressStart2P.ttf', 30)
     assets['font_gameover_texto'] = pygame.font.Font('assets/PressStart2P.ttf', 10)
     assets['font_inicio_titulo'] = pygame.font.Font('assets/PressStart2P.ttf', 20)
     assets['font_inicio_texto'] = pygame.font.Font('assets/PressStart2P.ttf', 15)
+    #Carregando as imagens
     assets['background'] = pygame.image.load('assets/background.jpeg').convert()
     assets['background_inicio'] = pygame.image.load('assets/background_inicio.png').convert()
     assets['galinha_img'] = pygame.image.load('assets/galinha.png').convert_alpha()
@@ -29,6 +36,7 @@ def load_assets():
     assets['carro3_contramao'] = pygame.transform.scale(carro3_contramao, (WIDTH_CARRINHO, HEIGHT_CARRINHO))
     assets['carro4_contramao'] = pygame.transform.scale(carro4_contramao, (WIDTH_CARRINHO, HEIGHT_CARRINHO))
 
+    #Carregando os sons
     assets['som_ponto'] = pygame.mixer.Sound('assets/sons/ponto.mpeg')
     assets['som_batida'] = pygame.mixer.Sound('assets/sons/batida.mpeg')
     assets['som_fim'] = pygame.mixer.Sound('assets/sons/ko.mp3')
