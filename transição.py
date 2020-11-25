52f7a9c863c302e3291813053e6f14edb255e4cc
RodrigoFurukawa <rodrigof7@al.insper.edu.br>
2020-11-25 00:25:18 -0300

    transicao 1

--- transição.py
+++ transição.py
@@ -1,1 +1,14 @@
 #abrindo arquivo
+import pygame
+import time
+
+
+def transition_screen(window):
+    font = pygame.font.Font('assets/PressStart2P.ttf', 20)
+    frase1 = font.render('A GALINHA FUGIU', False, (255, 255, 255))
+    frase2 = font.render('AJUDE ELA A ATRAVESSAR', False, (255, 255, 255))
+    frase3 = font.render('A RUA', False, (255, 255, 255))
+
+    clock = pygame.time.Clock()
+    FPS = 30
+    game = True
