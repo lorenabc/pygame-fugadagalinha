6938b3e10764976a0f6c5dee9c1dedb9d05fe8bf
RodrigoFurukawa <rodrigof7@al.insper.edu.br>
2020-11-25 20:05:09 -0300

    arquivo contendo constantes universais

---
+++ constantes.py
@@ -0,0 +1,31 @@
+#constantes de tamanho
+WIDTH = 500
+HEIGHT = 400
+WIDTH_CARRINHO = 80
+HEIGHT_CARRINHO = 40
+WIDTH_GALINHA = 30
+HEIGHT_GALINHA = 30
+FPS = 30
+
+#cores
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+AMARELO_CLARO = (255, 255, 102)
+BORDO = (220, 20, 60)
+
+#estados do arquivo carrega_tela_jogo
+ACABADO = -1
+INICIANDO = 0
+JOGANDO = 1
+ACABANDO = 2
+
+#estados do arquivo main
+QUIT = -1
+INICIO = 0
+TRANSICAO = 1
+JOGO = 2
+
+#estados do arquivo carrega_tela_inicial
+TELA_INICIAL = 0
+ANIMACAO = 1
+FIM = 2
