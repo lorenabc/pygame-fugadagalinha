cd485b9b8b4eb6468c02a50be903fa27e069b963
RodrigoFurukawa <rodrigof7@al.insper.edu.br>
2020-11-25 17:26:38 -0300

    classe galinha(do jogador)

--- classes.py
+++ classes.py
@@ -20,3 +20,32 @@ class GalinhaInicio(pygame.sprite.Sprite):
         # Atualização da posição da galinha
         self.rect.y += self.speedy
         self.y += self.speedy
+    
+    class GalinhaPlayer(pygame.sprite.Sprite):
+    def _init_(self, assets):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite._init_(self)
+
+        self.pontos = 0
+        self.image = assets['galinha_img']
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 3
+        self.rect.bottom = HEIGHT
+        self.speedy = 0
+        self.som = assets['som_ponto']
+
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.y += self.speedy
+
+        # Mantem dentro da tela
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+
+        # Se pontua volta para o inicio
+        if self.rect.top < 5:
+            self.som.play()
+            self.rect.bottom = HEIGHT
+            self.pontos += 1
+
